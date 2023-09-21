aae7c6c399cb38651bb49b0e9c0eec6a24b73759
kostya <70769021+kostyabet@users.noreply.github.com>
2023-09-21 22:16:00 +0300

    Update lab1.2.3.cpp

--- BlockOfLabs1/laboratory1.2/lab1.2.3.cpp
+++ BlockOfLabs1/laboratory1.2/lab1.2.3.cpp
@@ -6,28 +6,28 @@ int main()
     const int TABLESTEP = 50; // table step
     const int ENDOFTABLE = 1000; // top table border
     const int GRAMINKILO = 1000; // number of grams in a kilogram
-    int Cost = 0;
+    int cost = 0;
 
     for (int i = TABLESTEP; i < ENDOFTABLE + 1; i += TABLESTEP)
     {
         // current price calculation
-        Cost = (i * ONEKILOCOST) / GRAMINKILO;
+        cost = (i * ONEKILOCOST) / GRAMINKILO;
         // conclusion
         if (i < 99)
         {
-            std::cout << i << "   gram of cheese - cost: " << Cost << "  rubles.\n";
+            std::cout << i << "   gram of cheese - cost: " << cost << "  rubles.\n";
         }
-        else if (i < 999 && Cost < 100)
+        else if (i < 999 && cost < 100)
         {
-            std::cout << i << "  gram of cheese - cost: " << Cost << "  rubles.\n";
+            std::cout << i << "  gram of cheese - cost: " << cost << "  rubles.\n";
         }
         else if (i < 999)
         {
-            std::cout << i << "  gram of cheese - cost: " << Cost << " rubles.\n";
+            std::cout << i << "  gram of cheese - cost: " << cost << " rubles.\n";
         }
         else
         {
-            std::cout << i << " gram of cheese - cost: " << Cost << " rubles.\n";
+            std::cout << i << " gram of cheese - cost: " << cost << " rubles.\n";
         }
     }
     return 0;
