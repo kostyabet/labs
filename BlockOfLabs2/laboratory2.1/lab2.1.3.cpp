798f1a7f43e3a2bd7d672355385557263d721ae3
kostya <70769021+kostyabet@users.noreply.github.com>
2023-09-30 17:15:05 +0300

    commit cpp lab2.1

--- BlockOfLabs2/laboratory2.1/lab2.1.3.cpp
+++ BlockOfLabs2/laboratory2.1/lab2.1.3.cpp
@@ -4,28 +4,36 @@
 int main()
 {
     const float MINCOORDINATE = -1000.0, MAXCOORDINATE = 1000.0;
+    float area, slopeFactor, yInterception, productOfVectors1, productOfVectors2;
     const short MINNUMBEROFSIDE = 3, MAXNUMBEROFSIDE = 20;
-    int index;
     short numberOfSides, limitForAmount;
-    float area, slopeFactor, yInterception;
-    bool isInCorrect, isInCorrectCoordinate;
-    //inicialization
-    numberOfSides = 0;
-    area = 0.0;
-    limitForAmount = 0;
-    index = 0;
-    slopeFactor = 0;
-    yInterception = 0;
-    isInCorrect = true;
-    isInCorrectCoordinate = true;
-    //information about task
-    std::cout << "  This program calculates the area of\ a polygon. The number of sides of the polygon is selected by the user.\n"
-        "You also need to enter the coordinates of the polygon vertices.\n\n"
-        "Restrictions: \n\t1. The number of sides of a polygon is an integer from " << MINNUMBEROFSIDE << " to " << MAXNUMBEROFSIDE << ";\n"
-        "\t2. Coordinates - floating point numbers from " << MINCOORDINATE << " to " << MAXCOORDINATE << "; \n"
-        "\t3. The vertices of the polygon should be listed in traversal order (clockwise / counterclockwise).\n\n";
+    int index;
+    bool isInCorrect, isInCorrectCoordinate, isInCorrectAll, isCorrectInput;
 
+    //inicialization
+    area = 0.0;
+    slopeFactor = 0.0;//y = rx + b => slopefactor = r
+    yInterception = 0.0;//y = fx + b => slopefactor = b
+    productOfVectors1 = 0.0;//vector1;
+    productOfVectors2 = 0.0;//vector2;
+    limitForAmount = 0;//high in main block
+    index = 0;//x(index) and y(index)
+    numberOfSides = 0;
+    isInCorrect = true;//for input
+    isInCorrectCoordinate = true;//for coordinate cheack
+    isInCorrectAll = false;//for all block
+    isCorrectInput = false;//vector
+
+    //information about task
+    std::cout << "This program calculates the area of\ a polygon. The number of sides of the polygon is selected by the user.\n"
+        << "You also need to enter the coordinates of the polygon vertices.\n\n"
+        << "Restrictions: \n\t1. The number of sides of a polygon is an integer from " << MINNUMBEROFSIDE << " to " << MAXNUMBEROFSIDE << ";\n"
+        << "\t2. Coordinates - floating point numbers from " << MINCOORDINATE << " to " << MAXCOORDINATE << "; \n"
+        << "\t3. The vertices of the polygon should be listed in traversal order (clockwise / counterclockwise).\n\n";
+
+    //formatted output
     std::cout << std::setprecision(3) << std::fixed;
+
     //input number of sides
     do
     {
@@ -49,67 +57,107 @@ int main()
 
     //cin x and y
     float** coordinateMatrix = new float* [numberOfSides];
-    for (int i = 0; i < numberOfSides; i++)
+    do
     {
-        index = i + 1;
-        coordinateMatrix[i] = new float[2];
-        isInCorrectCoordinate = true;
-        do
+        isInCorrectAll = false;
+        isCorrectInput = false;
+        isInCorrect = false;
+        for (int i = 0; i < numberOfSides; i++)
         {
-            isInCorrect = true;
-            //cin x
+            index = i + 1;
+            coordinateMatrix[i] = new float[2];
+            isInCorrectCoordinate = true;
             do
             {
-                std::cout << "Write x" << index << ":\n";
-                std::cin >> coordinateMatrix[i][0];
-                if (std::cin.get() != '\n')
+                isInCorrect = true;
+                //cin x
+                do
                 {
-                    std::cin.clear();
-                    std::cin.ignore(30000, '\n');
-                    std::cout << "Error. Try again.\n";
-                }
-                else
-                    isInCorrect = false;
-            } while (isInCorrect);
-            //cin y
-            isInCorrect = true;
-            do
-            {
-                std::cout << "Write y" << index << ":\n";
-                std::cin >> coordinateMatrix[i][1];
-                if (std::cin.get() != '\n')
+                    std::cout << "\nWrite x" << index << ":\n";
+                    std::cin >> coordinateMatrix[i][0];
+                    if (std::cin.get() != '\n')
+                    {
+                        std::cin.clear();
+                        std::cin.ignore(30000, '\n');
+                        std::cout << "Error. Try again.\n";
+                    }
+                    else
+                        isInCorrect = false;
+                } while (isInCorrect);
+                //cin y
+                isInCorrect = true;
+                do
                 {
-                    std::cin.clear();
-                    std::cin.ignore(30000, '\n');
-                    std::cout << "Error. Try again.\n";
-                }
-                else
-                    isInCorrect = false;
-            } while (isInCorrect);
-            if (i > 1)
-            {
-                slopeFactor = (coordinateMatrix[i - 1][1] - coordinateMatrix[i - 2][1]) / (coordinateMatrix[i - 1][0] - coordinateMatrix[i - 2][0]);
-                yInterception = coordinateMatrix[i - 1][1] - coordinateMatrix[i - 1][0] * slopeFactor;
-                if (coordinateMatrix[i][1] == slopeFactor * coordinateMatrix[i][0] + yInterception)
+                    std::cout << "Write y" << index << ":\n";
+                    std::cin >> coordinateMatrix[i][1];
+                    if (std::cin.get() != '\n')
+                    {
+                        std::cin.clear();
+                        std::cin.ignore(30000, '\n');
+                        std::cout << "Error. Try again.\n";
+                    }
+                    else
+                        isInCorrect = false;
+                } while (isInCorrect);
+
+                if (i > 1)
                 {
-                    std::cout << "Three points cannot be on the same line. Try again.\n";
+                    slopeFactor = (coordinateMatrix[i - 1][1] - coordinateMatrix[i - 2][1]) / (coordinateMatrix[i - 1][0] - coordinateMatrix[i - 2][0]);
+                    yInterception = coordinateMatrix[i - 1][1] - coordinateMatrix[i - 1][0] * slopeFactor;
+                    if (coordinateMatrix[i][1] == slopeFactor * coordinateMatrix[i][0] + yInterception)
+                    {
+                        std::cout << "Three points cannot be on the same line. Try again.\n";
+                    }
+                    else
+                        isInCorrectCoordinate = false;
                 }
                 else
                     isInCorrectCoordinate = false;
+
+            } while (isInCorrectCoordinate);
+        }
+
+        //you need to implement a check for a regular polygon
+        /*if (numberOfSides > 3 && isInCorrect == false)
+        {
+            for (int i = 1; i < numberOfSides; i++)
+            {
+                productOfVectors1 = coordinateMatrix[i][0] * coordinateMatrix[i - 1][1] - coordinateMatrix[i][1] * coordinateMatrix[i - 1][0];
+                for (int j = i + 1; j < numberOfSides; j++)
+                {
+                    productOfVectors2 = coordinateMatrix[j][0] * coordinateMatrix[j - 1][1] - coordinateMatrix[j][1] * coordinateMatrix[j - 1][0];
+                    if ((productOfVectors1 == 0 || productOfVectors2 == 0 || productOfVectors1 == productOfVectors2) && isCorrectInput != true)
+                    {
+                        std::cout << "\nBad input. Try again.\n";
+                        isCorrectInput = true;
+                        isInCorrectAll = true;
+                    }
+                }
+            }
+        }*/
+
+        //main block
+        if (isInCorrectAll == false)
+        {
+            limitForAmount = numberOfSides - 1;
+            for (int i = 0; i < limitForAmount; i++)
+            {
+                area = area + (coordinateMatrix[i][0] * coordinateMatrix[i + 1][1]) - (coordinateMatrix[i + 1][0] * coordinateMatrix[i][1]);
+            }
+            area = abs(area + (coordinateMatrix[numberOfSides - 1][0] * coordinateMatrix[0][1]) - (coordinateMatrix[numberOfSides - 1][1] * coordinateMatrix[0][0]));
+            area = area / 2;
+            if (area == 0)
+            {
+                std::cout << "\nBad input. Try again.\n";
+                isInCorrectAll = true;
             }
             else
-                isInCorrectCoordinate = false;
+            {
+                std::cout << "\nYour area is: " << area << ".\n";
+            }
+        }
+    } while (isInCorrectAll);
 
-        } while (isInCorrectCoordinate);
-    }
-    //main block
-    limitForAmount = numberOfSides - 1;
-    for (int i = 0; i < limitForAmount; i++)
-    {
-        area = area + (coordinateMatrix[i][0] * coordinateMatrix[i + 1][1]) - (coordinateMatrix[i + 1][0] * coordinateMatrix[i][1]);
-    }
-    area = abs(area + (coordinateMatrix[numberOfSides - 1][0] * coordinateMatrix[0][1]) - (coordinateMatrix[numberOfSides - 1][1] * coordinateMatrix[0][0]));
-    area = area / 2;
     //cleaning the memory  
     for (int i = 0; i < numberOfSides; i++) {
         delete[] coordinateMatrix[i];
@@ -117,7 +165,6 @@ int main()
     }
     delete[] coordinateMatrix;
     coordinateMatrix = nullptr;
-    //cout area
-    std::cout << "\nYour area is: " << area << ".\n";
+
     return 0;
 }
