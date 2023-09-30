2cf4add4e0c956910f273d927d59470a62912135
kostya <70769021+kostyabet@users.noreply.github.com>
2023-09-30 14:58:59 +0300

    commit cpp lab2.1

--- BlockOfLabs2/laboratory2.1/lab2.1.3.cpp
+++ BlockOfLabs2/laboratory2.1/lab2.1.3.cpp
@@ -1,83 +1,106 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
-    const float MINCOORDINATE = -10000;
-    float MAXCOORDINATE = 10000;
-    short MINNUMBEROFSIDE = 3;
-    short MAXNUMBEROFSIDE = 127;
-    short numberOfSides;
-    short limitForAmount;
-    float area;
-    bool isCorrect;
+    const float MINCOORDINATE = -1000.0, MAXCOORDINATE = 1000.0;
+    const short MINNUMBEROFSIDE = 3, MAXNUMBEROFSIDE = 20;
+    int index;
+    short numberOfSides, limitForAmount;
+    float area, slopeFactor, yInterception;
+    bool isInCorrect, isInCorrectCoordinate;
     //inicialization
     numberOfSides = 0;
-    isCorrect = true;
-    area = 0;
+    area = 0.0;
     limitForAmount = 0;
+    index = 0;
+    slopeFactor = 0;
+    yInterception = 0;
+    isInCorrect = true;
+    isInCorrectCoordinate = true;
     //information about task
-    printf("  This program calculates the area of\ a polygon. The number of sides of the polygon is selected by the user.\n"
+    std::cout << "  This program calculates the area of\ a polygon. The number of sides of the polygon is selected by the user.\n"
         "You also need to enter the coordinates of the polygon vertices.\n\n"
-        "Restrictions: \n\t1. The number of sides of a polygon is an integer from %d to %d;\n"
-        "\t2. Coordinates - floating point numbers from %.1f to %.1f;\n"
-        "\t3. The vertices of the polygon should be listed in traversal order (clockwise / counterclockwise).\n\n", MINNUMBEROFSIDE, MAXNUMBEROFSIDE, MINCOORDINATE, MAXCOORDINATE);
+        "Restrictions: \n\t1. The number of sides of a polygon is an integer from " << MINNUMBEROFSIDE << " to " << MAXNUMBEROFSIDE << ";\n"
+        "\t2. Coordinates - floating point numbers from " << MINCOORDINATE << " to " << MAXCOORDINATE << "; \n"
+        "\t3. The vertices of the polygon should be listed in traversal order (clockwise / counterclockwise).\n\n";
 
+    std::cout << std::setprecision(3) << std::fixed;
     //input number of sides
-    printf("Write number of sides of a polygon:\n");
     do
     {
+        std::cout << "Write number of sides of a polygon:\n";
         std::cin >> numberOfSides;
         //cheack "Numeric input"
         if (std::cin.get() != '\n')
         {
             std::cin.clear();
             std::cin.ignore(30000, '\n');
-            printf("Error. Try again.\n");
+            std::cout << "Error. Try again.\n";
         }
         //cheack restrictions
         else if (numberOfSides < MINNUMBEROFSIDE || numberOfSides > MAXNUMBEROFSIDE)
         {
-            printf("Error. The number of sides of a polygon is an integer from %d to %d. Try again.\n", MINNUMBEROFSIDE, MAXNUMBEROFSIDE);
+            std::cout << "Error. The number of sides of a polygon is an integer from " << MINNUMBEROFSIDE << " to " << MAXNUMBEROFSIDE << ". Try again.\n";
         }
         else
-            isCorrect = false;
-    } while (isCorrect);
+            isInCorrect = false;
+    } while (isInCorrect);
 
     //cin x and y
     float** coordinateMatrix = new float* [numberOfSides];
     for (int i = 0; i < numberOfSides; i++)
     {
+        index = i + 1;
         coordinateMatrix[i] = new float[2];
-        isCorrect = true;
-        //cin x
+        isInCorrectCoordinate = true;
         do
         {
-            printf("Write x%d:\n", i + 1);
-            std::cin >> coordinateMatrix[i][0];
-            if (std::cin.get() != '\n')
+            isInCorrect = true;
+            //cin x
+            do
             {
-                std::cin.clear();
-                std::cin.ignore(30000, '\n');
-                printf("Error. Try again.\n");
+                std::cout << "Write x" << index << ":\n";
+                std::cin >> coordinateMatrix[i][0];
+                if (std::cin.get() != '\n')
+                {
+                    std::cin.clear();
+                    std::cin.ignore(30000, '\n');
+                    std::cout << "Error. Try again.\n";
+                }
+                else
+                    isInCorrect = false;
+            } while (isInCorrect);
+            //cin y
+            isInCorrect = true;
+            do
+            {
+                std::cout << "Write y" << index << ":\n";
+                std::cin >> coordinateMatrix[i][1];
+                if (std::cin.get() != '\n')
+                {
+                    std::cin.clear();
+                    std::cin.ignore(30000, '\n');
+                    std::cout << "Error. Try again.\n";
+                }
+                else
+                    isInCorrect = false;
+            } while (isInCorrect);
+            if (i > 1)
+            {
+                slopeFactor = (coordinateMatrix[i - 1][1] - coordinateMatrix[i - 2][1]) / (coordinateMatrix[i - 1][0] - coordinateMatrix[i - 2][0]);
+                yInterception = coordinateMatrix[i - 1][1] - coordinateMatrix[i - 1][0] * slopeFactor;
+                if (coordinateMatrix[i][1] == slopeFactor * coordinateMatrix[i][0] + yInterception)
+                {
+                    std::cout << "Three points cannot be on the same line. Try again.\n";
+                }
+                else
+                    isInCorrectCoordinate = false;
             }
             else
-                isCorrect = false;
-        } while (isCorrect);
-        //cin y
-        isCorrect = true;
-        do
-        {
-            printf("Write y%d:\n", i + 1);
-            std::cin >> coordinateMatrix[i][1];
-            if (std::cin.get() != '\n')
-            {
-                std::cin.clear();
-                std::cin.ignore(30000, '\n');
-                printf("Error. Try again.\n");
-            }
-            else
-                isCorrect = false;
-        } while (isCorrect);
+                isInCorrectCoordinate = false;
+
+        } while (isInCorrectCoordinate);
     }
     //main block
     limitForAmount = numberOfSides - 1;
@@ -85,10 +108,16 @@ int main()
     {
         area = area + (coordinateMatrix[i][0] * coordinateMatrix[i + 1][1]) - (coordinateMatrix[i + 1][0] * coordinateMatrix[i][1]);
     }
-
     area = abs(area + (coordinateMatrix[numberOfSides - 1][0] * coordinateMatrix[0][1]) - (coordinateMatrix[numberOfSides - 1][1] * coordinateMatrix[0][0]));
     area = area / 2;
+    //cleaning the memory  
+    for (int i = 0; i < numberOfSides; i++) {
+        delete[] coordinateMatrix[i];
+        coordinateMatrix[i] = nullptr;
+    }
+    delete[] coordinateMatrix;
+    coordinateMatrix = nullptr;
     //cout area
-    printf("Your area is: %.3f.", area);
+    std::cout << "\nYour area is: " << area << ".\n";
     return 0;
 }
