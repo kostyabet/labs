87e878926994b431ca556b2f539f83aed615415b
kostya <70769021+kostyabet@users.noreply.github.com>
2023-10-10 17:41:59 +0300

    lab2.2cpp

--- BlockOfLabs2/laboratory2.3/lab2.3.3.cpp
+++ BlockOfLabs2/laboratory2.3/lab2.3.3.cpp
@@ -1,15 +1,16 @@
 ﻿#include <iostream>
-#include <string>
+#include <string> // for write palindrom and way to the file
+#include <fstream> // for work with files
 using namespace std;
 
 
-int CinPalindrome() {
-	int num;
+string CinPalindrome() {
+	string str;
 	bool isCorrect = true;
 	do {
-		std::cin >> num;
+		cin >> str;
 		if (cin.fail() || cin.get() != '\n') {
-			std::cout << "Invalid numeric input.\nTry again.\n";
+			cout << "Invalid numeric input.\nTry again.\n";
 			cin.clear();
 			cin.ignore(30000, '\n');
 		}
@@ -17,22 +18,51 @@ int CinPalindrome() {
 			isCorrect = false;
 	} while (isCorrect);
 
-	return num;
+	return str;
+}
+
+
+string CinPalindromeTxt() {
+	string str = "";
+	getline(cin, str);
+	cout << endl << str << endl;
+	return str;
+}
+
+
+string CinTxt() {
+	string str;
+	bool isInCorrect = true;
+	do {
+		cin >> str;
+		if (str.size() > 4) {
+			string bufstr = str.substr(str.size() - 4, str.size() - 1);
+			if (bufstr == ".txt")
+				return str;
+			else
+				cout << "Write .txt file";
+		}
+		else
+			cout << "short string";
+	} while (isInCorrect);
+
+	return str;
 }
 
 
 int CinN() {
+	cout << "Your varient is: ";
 	int num;
 	bool isCorrect = true;
 	do {
-		std::cin >> num;
+		cin >> num;
 		if (cin.fail() || cin.get() != '\n') {
-			std::cout << "Invalid numeric input.\nTry again.\n";
+			cout << "Invalid numeric input.\nTry again.\n";
 			cin.clear();
 			cin.ignore(30000, '\n');
 		}
 		else if (num != 0 && num != 1)
-			std::cout << "choose only 1 or 2.\n";
+			cout << "choose only 1 or 2.\n";
 		else
 			isCorrect = false;
 	} while (isCorrect);
@@ -41,33 +71,51 @@ int CinN() {
 }
 
 
-int PalinSize(int n) {
-	int size = 0;
-	while (n) {
-		size++;
-		n /= 10;
+bool isPalindrom(string palindrom) {
+	for (int i = 0; i < palindrom.size() / 2; i++) {
+		if (palindrom[i] != palindrom[palindrom.size() - (i + 1)])
+			return false;
 	}
-
-	return size;
+	return true;
 }
 
 
-bool isPalindrom(int palindrom) {
-	int size = PalinSize(palindrom);
-
-
+string WorkWithTxt(string str) {
+	if (isPalindrom(str))
+		return "palindrom";
+	else
+		return "not a palindrom";
 }
 
 
 string ConsoleWork() {
-	cout << "\nWrite your number: ";
-	int palindrom = CinPalindrome();
+	cout << "\nWrite your string: ";
+	string palindrom = CinPalindrome();
 	if (isPalindrom(palindrom))
-		return "Palindrom";
+		return "palindrom";
 	else
-		return "Not a palindrom";
+		return "not a palindrom";
 }
 
+
+string FileWork() {
+	string fileway;
+	bool isCorrect = true;
+	do {
+		cout << "Write way to your file: ";
+		fileway = CinTxt();
+		ifstream file;
+		file.open(fileway);
+		if (file.is_open()) {
+			string str;
+			getline(file, str);
+			return WorkWithTxt(str);
+		}
+		else
+			cout << "Bad File. Try again.\n";
+		file.close();
+	} while (isCorrect);
+}
 
 int main() {
 	const int File = 1;
@@ -76,11 +124,12 @@ int main() {
 	int n = CinN();
 
 	string resoult;
-	if (n) {}
-	//resoult = FileWork();
+	if (n)
+		resoult = FileWork();
 	else
 		resoult = ConsoleWork();
 
-	cout << "Your number is " << resoult;
+	cout << "It is " << resoult;
+
 	return 0;
 }
