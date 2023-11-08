9fab313c550ac6d324b69f13427ee59b93196374
kostya <70769021+kostyabet@users.noreply.github.com>
2023-11-09 00:48:40 +0300

    newlab3.3.3cpp

--- BlockOfLabs3/laboratory3.3/lab3.3.3.cpp
+++ BlockOfLabs3/laboratory3.3/lab3.3.3.cpp
@@ -1,33 +1,41 @@
-﻿// метод простых вставок
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <fstream>
-
 using namespace std;
-
 const int FILE_KEY = 1;
 const int MIN_ARR_SIZE = 1;
 const int CONSOLE_KEY = 2;
 const int MIN_FILE_WAY_SIZE = 5;
 
+
+
+/// text blocks
 void conditionOutput()
 {
-	cout << "The program is designed to sort an array\n\t"
-		"using the simple insertion method.\n\n";
+	cout << "The program is designed to sort an array\n\tusing the simple insertion method.\n\n";
 }
-
 void pathConditionOutput()
 {
-	cout << "Where will we work through: \n\tFile: " <<
-		FILE_KEY << " Console: " << CONSOLE_KEY << endl << endl;
+	cout << "Where will we work through:\n\tFile: " << FILE_KEY << " Console: " << CONSOLE_KEY << endl << endl;
 }
-
 void fileRestriction()
 {
-	cout << "\n*The first number is the number of elements \n"
-		"of the array, and subsequent numbers of this array*\n";
+	cout << "\n*The first number is the number of elements\nof the array, and subsequent numbers of this array*\n";
 }
 
+
+
+/// is can open
+bool isCanOpenFile(string way, ios_base::openmode mode)
+{
+	fstream file(way, mode);
+	file.close();
+	return file.good();
+}
+
+
+
+/// input way of work
 int choosingAPath()
 {
 	int path = 0;
@@ -56,6 +64,53 @@ int choosingAPath()
 	return path;
 }
 
+
+
+/// input way to the file
+bool pathCondition(string way)
+{
+	if (way.size() < MIN_FILE_WAY_SIZE)
+	{
+		cerr << "The path is too short. Try again: ";
+		return false;
+	}
+	string bufstr = way.substr(way.size() - 4);
+	if (bufstr != ".txt")
+	{
+		cerr << "Write .txt file. Try again: ";
+		return false;
+	}
+	return true;
+}
+string inputPath()
+{
+	string way;
+	bool isIncorrect;
+	do
+	{
+		cin >> way;
+		isIncorrect = !pathCondition(way);
+	} while (isIncorrect);
+	return way;
+}
+string inputPathToTheFile()
+{
+	string fileWay;
+	bool isIncorrect = true;
+	do
+	{
+		bool isCorrect = true;
+		fileWay = inputPath();
+		if (!isCanOpenFile(fileWay, ios::in))
+			cout << "Can't open a file. Try write another way: ";
+		else
+			isIncorrect = false;
+	} while (isIncorrect);
+	return fileWay;
+}
+
+
+
 /// input from console
 int arrSizeInputFromConsole()
 {
@@ -81,7 +136,6 @@ int arrSizeInputFromConsole()
 
 	return arrSize;
 }
-
 int inputCurrentNumbFromConsole()
 {
 	int currentNumb;
@@ -101,105 +155,65 @@ int inputCurrentNumbFromConsole()
 
 	return currentNumb;
 }
-
-void arrOfNumbInputFromConsole(int arrSize, int*& arrOfNumb)
+bool isCorrectArrOfNumbInputFromConsole(int arrSize, int*& arrOfNumb)
 {
 	for (int i = 0; i < arrSize; i++)
 	{
 		cout << "Write your " << i + 1 << " number: ";
 		arrOfNumb[i] = inputCurrentNumbFromConsole();
 	}
+
+	return false;
 }
+
+
 
 /// input from file
-bool isCanOpenFile(string way, ios_base::openmode mode)
+int arrSizeInputFromFile(string fileWay)
 {
-	fstream file(way, mode);
-	file.close();
-	return file.good();
-}
+	int arrSize;
+	ifstream file(fileWay, ios::in);
+	file >> arrSize;
 
-bool wayCondition(string way)
-{
-	if (way.size() < MIN_FILE_WAY_SIZE)
-	{
-		cerr << "The path is too short. Try again: ";
-		return false;
-	}
-	string bufstr = way.substr(way.size() - 4);
-	if (bufstr != ".txt")
-	{
-		cerr << "Write .txt file. Try again: ";
-		return false;
-	}
-	return true;
-}
-
-string inputWayToTheFile()
-{
-	string way;
-	bool isIncorrect;
-	do
-	{
-		cin >> way;
-		isIncorrect = !wayCondition(way);
-	} while (isIncorrect);
-	return way;
-}
-
-
-string inputFile()
-{
-	string fileWay;
-	bool isIncorrect = true;
-	do
-	{
-		bool isCorrect = true;
-		fileWay = inputWayToTheFile();
-		if (!isCanOpenFile(fileWay, ios::in))
-			cout << "Can't open a file. Try write another way: ";
-		else
-			isIncorrect = false;
-	} while (isIncorrect);
-	return fileWay;
-}
-
-bool arrSizeInputFromFile(ifstream& file, int arrSize)
-{
-	bool isIncorrect = true;
 	if (file.fail() || file.get() != '\n')
+	{
 		cout << "Error in input size of massive. Try again: ";
+		arrSize = -1;
+	}
 	else if (arrSize < MIN_ARR_SIZE)
+	{
 		cout << "Minimal arr size is " << MIN_ARR_SIZE << ". Try again: ";
-	else
-		isIncorrect = false;
+		arrSize = -1;
+	}
 
-	return isIncorrect;
+	file.close();
+
+	return arrSize;
 }
-
-bool isIncorrectInputCurrentNumbFromFile(ifstream& file)
+bool isIncorrectArrOfNumbInputFromFile(int arrSize, int*& arrOfNumb, string fileWay)
 {
-	bool isIncorrect = false;
-	if (file.fail())
-		isIncorrect = true;
+	if (arrSize == -1)
+		return true;
 
-	return isIncorrect;
-}
-
-bool isIncorrectArrOfNumbInputFromFile(ifstream& file, int*& arrOfNumb,
-	int arrSize)
-{
-	bool isIncorrect = false;
+	ifstream file(fileWay, ios::in);
 	for (int i = 0; i < arrSize; i++)
 	{
 		file >> arrOfNumb[i];
-		isIncorrect = isIncorrectInputCurrentNumbFromFile(file);
+		if (file.fail())
+		{
+			file.close();
+			cout << "Error in reading massive elements. Try again: ";
+			return true;
+		}
 	}
+	file.close();
 
-	return isIncorrect;
+
+	return false;
 }
 
-/// result calc.. 
+
+
 void sortMassive(int*& arrOfNumb, int arrSize)
 {
 	int temp; // временная переменная для хранения значения элемента сортируемого массива
@@ -218,14 +232,16 @@ void sortMassive(int*& arrOfNumb, int arrSize)
 	}
 }
 
-/// output from file
+
+
+/// output block
 void outputFromFile(int* arrOfNumb, int arrSize)
 {
 	bool isIncorrect = true;
 	cout << "Write way to your file: ";
 	do
 	{
-		string fileWay = inputWayToTheFile();
+		string fileWay = inputPath();
 		if (isCanOpenFile(fileWay, ios::out))
 		{
 			ofstream file(fileWay, ios::out);
@@ -239,60 +255,46 @@ void outputFromFile(int* arrOfNumb, int arrSize)
 			cerr << "Can't open a file. Try write another way: ";
 	} while (isIncorrect);
 }
-
-/// output from console
 void outputFromConsole(int* arrOfNumb, int arrSize)
 {
 	for (int i = 0; i < arrSize; i++)
 		cout << arrOfNumb[i] << " ";
 }
-
-/// output
 void resultOutput(int* arrOfNumb, int arrSize)
 {
 	cout << "You need to choose where to write information from.\n";
 	int path = choosingAPath();
-	path == CONSOLE_KEY ? outputFromConsole(arrOfNumb, arrSize)
-		: outputFromFile(arrOfNumb, arrSize);
+
+	path == FILE_KEY ? outputFromFile(arrOfNumb, arrSize) : outputFromConsole(arrOfNumb, arrSize);
+}
+
+
+
+int arrSizeInput(int path, string fileWay) {
+	return path == FILE_KEY ? arrSizeInputFromFile(fileWay) : arrSizeInputFromConsole();
+}
+
+bool inputArrOfNumb(int arrSize, int*& arrOfNumb, int path, string fileWay) {
+	return path == FILE_KEY ? isIncorrectArrOfNumbInputFromFile(arrSize, arrOfNumb, fileWay) : isCorrectArrOfNumbInputFromConsole(arrSize, arrOfNumb);
 }
 
 int main() {
-	int arrSize = 0;
-	int* arrOfNumb = new int[0];
+	int arrSize;
+	int* arrOfNumb;
 
 	conditionOutput();
 
 	cout << "\nYou need to choose where to read information from.\n";
-
 	int path = choosingAPath();
-	if (path == CONSOLE_KEY) {
-		arrSize = arrSizeInputFromConsole();
 
+	bool isIncorrect;
+	do {
+		string fileWay = path == FILE_KEY ? inputPathToTheFile() : "";
+
+		arrSize = arrSizeInput(path, fileWay);
 		arrOfNumb = new int[arrSize];
-		arrOfNumbInputFromConsole(arrSize, arrOfNumb);
-	}
-	else {
-		bool isIncorrect = true;
-		fileRestriction();
-		cout << "Write way to your file: ";
-		do
-		{
-			string fileWay = inputFile();
-			ifstream file(fileWay, ios::in);
-			file >> arrSize;
-			isIncorrect = arrSizeInputFromFile(file, arrSize);
-			if (!isIncorrect)
-			{
-				arrOfNumb = new int[arrSize];
-				isIncorrect = isIncorrectArrOfNumbInputFromFile(file,
-					arrOfNumb, arrSize);
-				if (isIncorrect)
-					cout << "Invalid massive elements input. Try again: ";
-			}
-
-			file.close();
-		} while (isIncorrect);
-	}
+		isIncorrect = inputArrOfNumb(arrSize, arrOfNumb, path, fileWay);
+	} while (isIncorrect);
 
 	sortMassive(arrOfNumb, arrSize);
 
