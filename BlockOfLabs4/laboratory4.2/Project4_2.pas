de6f30344fbd2da927e1345072623d1f77aca732
Kostya <betenya.kostya@mail.ru>
2024-01-24 19:32:29 +0300

    start form create

---
+++ BlockOfLabs4/laboratory4.2/Project4_2.pas
@@ -0,0 +1,39 @@
+Unit Project4_2;
+
+Interface
+
+Uses
+    Winapi.Windows,
+    Winapi.Messages,
+    System.SysUtils,
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs;
+
+Type
+    TForm5 = Class(TForm)
+    Private
+        { Private declarations }
+    Public
+        { Public declarations }
+    End;
+
+Var
+    Form5: TForm5;
+
+Type
+    TMassive = Array Of Integer;
+
+Implementation
+
+{$R *.dfm}
+
+Procedure SearchSum(Massive: TMassive; I: Integer);
+Begin
+
+End;
+
+End.
