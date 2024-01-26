0770bb61d7f4be3fd375df740387261f73b4d60b
Kostya <betenya.kostya@mail.ru>
2024-01-26 17:10:58 +0300

    .gitignore update: add C# control and .vs cache

--- BlockOfLabs4/laboratory4.2/C#/Proj4_2/C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ BlockOfLabs4/laboratory4.2/C#/Proj4_2/C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44235630764b9f652f8d4f21c698adcfd43c7a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fa425d0f963fc784ff62bcdd73fb146fd3b64b")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
