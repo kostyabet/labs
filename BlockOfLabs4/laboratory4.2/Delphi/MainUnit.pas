e10cd58fa03dcf1d023b5660b1d5c57e34b2643e
Kostya <betenya.kostya@mail.ru>
2024-01-28 20:22:34 +0300

    A input verification added

--- BlockOfLabs4/laboratory4.2/Delphi/MainUnit.pas
+++ BlockOfLabs4/laboratory4.2/Delphi/MainUnit.pas
@@ -75,7 +75,10 @@ Procedure TMainForm.ALabeledEditKeyDown(Sender: TObject; Var Key: Word; Shift: T
 Begin
     TEdit(Sender).ReadOnly := (SsShift In Shift) Or (SsCtrl In Shift);
 
-    If Key = VK_DOWN Then
+    If (Key = VK_BACK) Or (Key = VK_DELETE) Then
+        Key := TryToDelete(Key, ALabeledEdit.Text, ALabeledEdit.SelStart, ALabeledEdit);
+
+    If (Key = VK_DOWN) Or (Key = VK_RETURN) Then
         SelectNext(ActiveControl, True, True);
 
     If Key = VK_UP Then
@@ -85,34 +88,21 @@ End;
 Procedure TMainForm.ALabeledEditKeyPress(Sender: TObject; Var Key: Char);
 Const
     GOOD_VALUES: Set Of Char = ['0' .. '9'];
-    MAX_A_LENGTH: Integer = 4;
+    MAX_A_LENGTH: Integer = 9;
     NULL_POINT: Char = #0;
 Var
     MinCount: Integer;
 Begin
     MinCount := 0;
 
-    If ((Length(ALabeledEdit.Text) <> 0) And (ALabeledEdit.Text[1] = '-')) Then
+    If ((Length(ALabeledEdit.Text) <> 0) And (ALabeledEdit.Text[1] = '-')) Or (Key = '-') Then
         MinCount := 1;
 
-    If (Key = '-') And (ALabeledEdit.SelStart = 0) And (ALabeledEdit.Text <> '') And (ALabeledEdit.Text[1] = '-') Then
+    If Not(TryToAdd(Key, ALabeledEdit.Text, ALabeledEdit.SelStart, MAX_INT_NUM, MIN_INT_NUM)) Then
         Key := NULL_POINT;
 
-    If (ALabeledEdit.SelStart = 0) And (Length(ALabeledEdit.Text) = MAX_A_LENGTH + MinCount - 1) And ((Key <> '1') or (Key <> '-')) Then
-        Key := NULL_POINT;
+    Key := CheckMinusOrZero(Key, NULL_POINT, ALabeledEdit);
 
-    If Not((Key In GOOD_VALUES) Or (Key = '-')) Then
-        Key := NULL_POINT;
-
-    If (Key = '-') And (ALabeledEdit.SelStart <> 0) Then
-        Key := NULL_POINT;
-
-    If Not((Key In GOOD_VALUES) Or (Key = '-')) Then
-        Key := NULL_POINT;
-
-    if (Key = '-') then
-        MinCount := 1;
-        
     If (ALabeledEdit.SelText <> '') And (Key <> NULL_POINT) Then
         ALabeledEdit.ClearSelection
     Else
