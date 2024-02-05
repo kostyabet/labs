e86d49ca6e80a6f2bead3f6e282044a4ee672e54
Kostya <betenya.kostya@mail.ru>
2024-02-05 10:27:28 +0300

    Backend file update: include Records

--- BlockOfLabs4/laboratory4.1/Delphi/BackendUnit.pas
+++ BlockOfLabs4/laboratory4.1/Delphi/BackendUnit.pas
@@ -1,10 +1,23 @@
-Unit BackendUnit;
+﻿Unit BackendUnit;
 
 Interface
 
 Uses
+    Clipbrd,
+    Winapi.Windows,
+    Winapi.Messages,
     System.SysUtils,
-    Vcl.Grids;
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs,
+    Vcl.Menus,
+    Vcl.StdCtrls,
+    Vcl.Mask,
+    Vcl.Grids,
+    Vcl.ExtCtrls;
 
 Type
     TFootballStats = Record
@@ -14,43 +27,156 @@ Type
         Points: Integer;
     End;
 
+    TMassive = Array Of Integer;
     TFootballMassive = Array Of TFootballStats;
 
-Procedure InputDataInMassive(Country, Team, Coach, Points: String);
-Procedure InputMassiveInTableGrid(FootballTable: TFootballMassive; Var PointTabelStrGrid: TStringGrid);
+Procedure InputDataInMassive(Country, Team, Coach, Points: String; CurentRow: Integer);
+Procedure InputMassiveInTableGrid();
+Procedure InputInfoFromGrid(Var CountryLabeledEdit, TeamNameLabeledEdit, CoachLabeledEdit, PointsLabeledEdit: TLabeledEdit; I: Integer);
+Procedure SortFootballStats();
+Procedure DeleteRow(I: Integer);
+Function IndexRecord(I: Integer; CurentStr: String): Integer;
+Function CreateResultGrid(StrIndex: Integer): String;
 
 Var
     FootballTable: TFootballMassive;
 
 Implementation
 
-Procedure InputDataInMassive(Country, Team, Coach, Points: String);
+Uses
+    MainFormUnit,
+    FrontendUnit;
+
+Procedure InputDataInMassive(Country, Team, Coach, Points: String; CurentRow: Integer);
 Begin
-    SetLength(FootballTable, Length(FootballTable) + 1);
-    FootballTable[High(FootballTable)].Country := Country;
-    FootballTable[High(FootballTable)].Team := Team;
-    FootballTable[High(FootballTable)].Coach := Coach;
-    FootballTable[High(FootballTable)].Points := StrToInt(Points);
+    FootballTable[CurentRow].Country := Country;
+    FootballTable[CurentRow].Team := Team;
+    FootballTable[CurentRow].Coach := Coach;
+    FootballTable[CurentRow].Points := StrToInt(Points);
 End;
 
-Procedure InputMassiveInTableGrid(FootballTable: TFootballMassive; Var PointTabelStrGrid: TStringGrid);
+Procedure InputMassiveInTableGrid();
 Var
-    I, J: Integer;
+    I: Integer;
 Begin
-    PointTabelStrGrid.RowCount := High(FootballTable) + 2;
+    MainForm.PointTabelStrGrid.RowCount := High(FootballTable) + 2;
 
-    Try
-        For I := 1 To High(FootballTable) + 2 Do
-        Begin
-            PointTabelStrGrid.Cells[0, I] := IntToStr(I);
-            PointTabelStrGrid.Cells[1, I] := FootballTable[I - 1].Country;
-            PointTabelStrGrid.Cells[2, I] := FootballTable[I - 1].Team;
-            PointTabelStrGrid.Cells[3, I] := FootballTable[I - 1].Coach;
-            PointTabelStrGrid.Cells[4, I] := IntToStr(FootballTable[I - 1].Points);
-        End;
-    Except
+    For I := 1 To High(FootballTable) + 2 Do
+        If Not(I - 1 > High(FootballTable)) Then
+            InputInCurentRow(I, FootballTable[I - 1].Country, FootballTable[I - 1].Team, FootballTable[I - 1].Coach,
+                FootballTable[I - 1].Points);
+End;
 
+Procedure InputInfoFromGrid(Var CountryLabeledEdit, TeamNameLabeledEdit, CoachLabeledEdit, PointsLabeledEdit: TLabeledEdit; I: Integer);
+Begin
+    If Not(I - 1 > High(FootballTable)) Then
+    Begin
+        CountryLabeledEdit.Text := FootballTable[I - 1].Country;
+        TeamNameLabeledEdit.Text := FootballTable[I - 1].Team;
+        CoachLabeledEdit.Text := FootballTable[I - 1].Coach;
+        PointsLabeledEdit.Text := IntToStr(FootballTable[I - 1].Points);
     End;
+End;
+
+Procedure SortFootballStats();
+Var
+    TempP, I, J: Integer;
+    TempT, TempCh, TempCy: String;
+Begin
+    For I := 1 To High(FootballTable) Do
+    Begin
+        TempP := FootballTable[I].Points;
+        TempT := FootballTable[I].Team;
+        TempCh := FootballTable[I].Coach;
+        TempCy := FootballTable[I].Country;
+
+        J := I - 1;
+        While (J >= 0) And (FootballTable[J].Points < TempP) Do
+        Begin
+            FootballTable[J + 1].Points := FootballTable[J].Points;
+            FootballTable[J + 1].Team := FootballTable[J].Team;
+            FootballTable[J + 1].Coach := FootballTable[J].Coach;
+            FootballTable[J + 1].Country := FootballTable[J].Country;
+
+            FootballTable[J].Points := TempP;
+            FootballTable[J].Team := TempT;
+            FootballTable[J].Coach := TempCh;
+            FootballTable[J].Country := TempCy;
+
+            Dec(J);
+        End;
+    End;
+End;
+
+Procedure DeleteRow(I: Integer);
+Begin
+    While Not(I > High(FootballTable) - 1) Do
+    Begin
+        FootballTable[I].Country := FootballTable[I + 1].Country;
+        FootballTable[I].Team := FootballTable[I + 1].Team;
+        FootballTable[I].Coach := FootballTable[I + 1].Coach;
+        FootballTable[I].Points := FootballTable[I + 1].Points;
+        Inc(I);
+    End;
+    SetLength(FootballTable, Length(FootballTable) - 1);
+    InputMassiveInTableGrid();
+End;
+
+Function IndexRecord(I: Integer; CurentStr: String): Integer;
+Var
+    J: Integer;
+Begin
+    IndexRecord := -1;
+    Case I Of
+        0:
+            Begin
+                For J := Low(FootballTable) To High(FootballTable) Do
+                    If (FootballTable[J].Country = CurentStr) Then
+                    Begin
+                        IndexRecord := J;
+                        Exit;
+                    End;
+            End;
+        1:
+            Begin
+                For J := Low(FootballTable) To High(FootballTable) Do
+                    If (FootballTable[J].Team = CurentStr) Then
+                    Begin
+                        IndexRecord := J;
+                        Exit;
+                    End;
+            End;
+        2:
+            Begin
+                For J := Low(FootballTable) To High(FootballTable) Do
+                    If (FootballTable[J].Coach = CurentStr) Then
+                    Begin
+                        IndexRecord := J;
+                        Exit;
+                    End;
+            End;
+        3:
+            Begin
+                For J := Low(FootballTable) To High(FootballTable) Do
+                    If (IntToStr(FootballTable[J].Points) = CurentStr) Then
+                    Begin
+                        IndexRecord := J;
+                        Exit;
+                    End;
+            End;
+    End;
+End;
+
+Function CreateResultGrid(StrIndex: Integer): String;
+Var
+    ResStr: String;
+Begin
+    ResStr := 'Страна: ' + FootballTable[StrIndex].Country + #13#10;
+    ResStr := ResStr + 'Название команды: ' + FootballTable[StrIndex].Team + #13#10;
+    ResStr := ResStr + 'Гл. Тренер: ' + FootballTable[StrIndex].Coach + #13#10;
+    ResStr := ResStr + 'Итоговый результат:' + IntToStr(FootballTable[StrIndex].Points);
+
+    CreateResultGrid := ResStr;
 End;
 
 End.
