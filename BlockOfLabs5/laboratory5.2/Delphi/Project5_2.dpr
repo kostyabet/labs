c49325e9ca387b614ea12b5eb80567cbb3187bf1
Kostya <betenya.kostya@mail.ru>
2024-02-18 16:44:47 +0300

    Start Basic form create

---
+++ BlockOfLabs5/laboratory5.2/Delphi/Project5_2.dpr
@@ -0,0 +1,14 @@
+program Project5_2;
+
+uses
+  Vcl.Forms,
+  MainFormUnit in 'MainFormUnit.pas' {Form5};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm5, Form5);
+  Application.Run;
+end.
