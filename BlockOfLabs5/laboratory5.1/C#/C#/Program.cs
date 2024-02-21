41124349bedfbae600c6daef919d731a44798849
Kostya <betenya.kostya@mail.ru>
2024-02-21 14:37:56 +0300

    create default console aplication for lab 51/

---
+++ BlockOfLabs5/laboratory5.1/C#/C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Proj4_2
+{
+    static void outputCondition()
+    {
+        Console.WriteLine($"""
+            Condition;
+        """);
+    }
+    static void mainWorkBlock()
+    {
+        
+    }
+    static void Main(String[] args)
+    {
+        outputCondition();
+        mainWorkBlock();
+        Environment.Exit(0);
+    }
+}
