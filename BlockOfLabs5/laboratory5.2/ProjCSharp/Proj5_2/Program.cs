04348da2d271806ec36b3eeca997d1533ce9442e
Kostya <betenya.kostya@mail.ru>
2024-02-23 22:15:26 +0300

    some changes create

---
+++ BlockOfLabs5/laboratory5.2/ProjCSharp/Proj5_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// ToDo: passive voice
+
+namespace Proj5_2
+{
+    class Program
+    {
+        public static void Main(){
+            int num = 1;
+            Console.WriteLine("Hello world");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+        }
+    }
+}
