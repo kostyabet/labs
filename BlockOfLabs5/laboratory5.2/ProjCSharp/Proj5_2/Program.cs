4e1ea75ebf25976baacc52b7ede9d92c63941dc8
Kostya <betenya.kostya@mail.ru>
2024-02-26 12:23:24 +0300

    output and treatment create. Vertical output create.

--- BlockOfLabs5/laboratory5.2/ProjCSharp/Proj5_2/Program.cs
+++ BlockOfLabs5/laboratory5.2/ProjCSharp/Proj5_2/Program.cs
@@ -1,13 +1,15 @@
 ﻿namespace Proj5_2 {
     public class Node {
         public readonly int Data;
+        public Node? Parent;
         public int LCost;
         public int RCost;
         public Node? Left;
         public Node? Right;
 
-        public Node(int data) {
+        public Node(int data, Node? parent) {
             Data = data;
+            Parent = parent;
             Left = null;
             LCost = 0;
             RCost = 0;
@@ -31,9 +33,24 @@
         private static int _minChild = 1;
         private static int _maxChild = 39;
         private const int MaxPointsCount = 40;
+        private static Node? _longestPoint = _root;
+        private static int _longWayCost = 0;
         private static readonly bool[] IsParentExist = new bool[MaxPointsCount];
-        private static void InorderTraversal() {
-            InorderTraversal(_root, 0);
+        private static void PrintTree(Node root, string prefix = "", bool isLeft = false)
+        {
+            if (root == null)
+                return;
+
+            Console.WriteLine(prefix + (isLeft ? "├── " : "└── ") + root.Data);
+
+            PrintTree(root.Left, prefix + (isLeft ? "│   " : "    "), true);
+
+            PrintTree(root.Right, prefix + (isLeft ? "│   " : "    "), false);
+        }
+        private static void OutputTree()
+        {
+            PrintTree(_root);
+            //InorderTraversal(_root, 0);
         }
 
         static void InorderTraversal(Node? root, int cost)
@@ -61,53 +78,56 @@
             return num;
         }
 
-        static void Search(ref Node? root, int value) {
-            if (root != null) {
-                if (root.Data == value)
-                    _root = root;
-                Search(ref root.Left, value);
-                if (root.Data == value)
-                    _root = root;
-                Search(ref root.Right, value);
-            }
+        static void Search(ref Node? root, int value)
+        {
+            if (root == null) return;
+            if (root.Data == value)
+                _root = root;
+            Search(ref root.Left, value);
+            if (root.Data == value)
+                _root = root;
+            Search(ref root.Right, value);
         }
         
-        static void Insert(int parent, int child, int cost) {
+        static void InsertNewBranch(int parent, int child, int cost) {
             Node? temp = _root;
             Search(ref temp, parent);
-            _root = InsertNode(_root, child, cost);
+            _root = InsertProcess(_root, _root, child, cost);
         }
 
-        private static Node InsertNode(Node? root, int child, int cost) {
+        private static Node InsertProcess(Node? root, Node? parent, int child, int cost) {
             if (root == null) {
                 IsParentExist[child] = true;
-                root = new Node(child);
+                root = new Node(child, parent);
                 return root;
             }
 
             if (root.Left == null) {
                 root.LCost = cost;
-                root.Left = InsertNode(root.Left, child, cost);   
+                root.Left = InsertProcess(root.Left, root, child, cost);   
             }
             else {
                 root.RCost = cost;
-                root.Right = InsertNode(root.Right, child, cost);
+                root.Right = InsertProcess(root.Right, root, child, cost);
             }
 
             return root;
         }
 
-        static int InputParentNum() {
+        private static int InputParentNum() {
             Console.Write("Input parent index: ");
-            int num;
+            int parent;
             bool isCorrect;
             do {
-                num = InputNum(ref _maxParent, ref _minParent);
-                isCorrect = IsParentExist[num];
+                parent = InputNum(ref _maxParent, ref _minParent);
+                Node? temp = _root;
+                Search(ref temp, parent);
+                isCorrect = IsParentExist[parent] && !(_root is { Left: not null, Right: not null });
+                _root = temp;
                 if (!isCorrect) Console.Write("Error. You should write parent index that exist: ");
             } while (!isCorrect);
 
-            return num;
+            return parent;
         }
 
         private static int InputChildNum() {
@@ -122,7 +142,7 @@
             return num;
         }
 
-        static bool IsPersonWantContinue()
+        static bool IsWriterWantContinue()
         {
             Console.WriteLine("1 - exit; 2 - write new branch;");
             Console.Write("Do you want write one more branch: ");
@@ -133,27 +153,52 @@
             };
         }
 
-        private static void InputProcess()
+        private static void InputBranch(out int parent, out int child, out int cost)
+        {
+            parent = InputParentNum();
+            child = InputChildNum();
+            Console.Write("How much this branch cost: ");
+            cost = InputNum(ref _maxCost, ref _minCost);
+        }
+        private static void InputTree()
         {
             IsParentExist[1] = true;
-            _root = new Node(1);
-            Node temp = _root;
+            _root = new Node(1, null);
+            Node head = _root;
             do {
-                var parent = InputParentNum();
-                var child = InputChildNum();
-                Console.Write("Write cost ofo this branch: ");
-                var cost = InputNum(ref _maxCost, ref _minCost);
-                Insert(parent,child,cost);
-                _root = temp;
-            } while (IsPersonWantContinue());
+                InputBranch(out int parent, out int child, out int cost);
+                InsertNewBranch(parent,child,cost);
+                _root = head;
+            } while (IsWriterWantContinue());
         }
 
-        public static void Main()
+        private static void SearchLongestWay(Node? root, Node? pastRoot, int cost)
         {
-            InputProcess();
-            Console.WriteLine("Inorder Traversal:"); 
-            InorderTraversal();
-            Console.WriteLine();
+            if (root == null) return;
+            SearchLongestWay(root.Left, root, cost + root.LCost);
+            if (cost > _longWayCost) {
+                _longWayCost = cost;
+                _longestPoint = root;
+            }
+            SearchLongestWay(root.Right, root, cost + root.RCost);
+        }
+
+        static void ToMirrorTree() {
+            while (_longestPoint?.Parent != null) 
+                _longestPoint = _longestPoint.Parent;
+            
+            Node temp = _longestPoint.Right;
+            _longestPoint.Right = _longestPoint.Left;
+            _longestPoint.Left = temp;
+        }
+        private static void TreatmentTree() {
+            SearchLongestWay(_root, _root, 0);
+            ToMirrorTree();
+        }
+        public static void Main() {
+            InputTree();
+            TreatmentTree();
+            OutputTree();
         }
     }
 }
